e5ee83e4c390e0c6f21e59ba10bcdb4dee9e0bfb
zw29n47bmb-dev <zw29n47bmb@privaterelay.appleid.com>
2026-03-24 15:02:54 -0700

    Implement stack functions using list operations

--- pila/pila.c
+++ pila/pila.c
@@ -2,48 +2,39 @@
 
 Pila* pila_crear()
 {
-    /*
-    TODO
-    usar crear lista
-    */
+    return lista_crear();
 }
 
 int pila_vacia(Pila* pila)
 {
-    /*
-    TODO
-    usar lista_vacia
-    */
+    return lista_vacia(pila);
 }
 
 void pila_push(Pila* pila, int dato)
 {
-    /*
-    TODO
-    usar lista_insertar_tail
-    */
+    // insertar al final (tail)
+    lista_insertar_tail(pila, dato);
 }
 
 int pila_pop(Pila* pila)
 {
-    /*
-    TODO
-    usar lista_eliminar_tail
-    */
-    return -1;
+    // eliminar del final (tail)
+    if (pila_vacia(pila))
+        return -1;
+
+    return lista_eliminar_tail(pila);
 }
 
 int pila_top(Pila* pila)
 {
-    /*
-    TODO
-    regresar dato del tail
-    */
-    return -1;
+    // regresar dato del tail
+    if (pila_vacia(pila))
+        return -1;
+
+    return pila->tail->dato;
 }
 
 void pila_destruir(Pila* pila)
 {
-    // TODO
-    // Usar lista destruir
+    lista_destruir(pila);
 }
