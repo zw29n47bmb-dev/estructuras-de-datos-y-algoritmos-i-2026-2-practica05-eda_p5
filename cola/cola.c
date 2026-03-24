b8811b4c7691acad2545a7fcf83fe462e0196317
zw29n47bmb-dev <zw29n47bmb@privaterelay.appleid.com>
2026-03-24 14:40:11 -0700

    Implement queue functions using list operations

--- cola/cola.c
+++ cola/cola.c
@@ -7,42 +7,31 @@ Cola* cola_crear()
 
 int cola_vacia(Cola* cola)
 {
-    /*
-    TODO
-    usar vaciar_lista
-    */
+    return lista_vacia(cola);
 }
 
 void cola_enqueue(Cola* cola, int dato)
 {
-    /*
-    TODO
-    usar lista_insertar_tail
-    */
+    lista_insertar_tail(cola, dato);
 }
 
 int cola_dequeue(Cola* cola)
 {
-    /*
-    TODO
-    usar lista_eliminar_head
-    */
-    return -1;
+    if (cola_vacia(cola))
+        return -1;
+
+    return lista_eliminar_head(cola);
 }
 
 int cola_frente(Cola* cola)
 {
-    /*
-    TODO
-    regresar dato del head
-    */
-    return -1;
+    if (cola_vacia(cola))
+        return -1;
+
+    return lista_ver_head(cola);
 }
 
 void cola_destruir(Cola* cola)
 {
-    /*
-    TODO
-    usar lista_destruir
-    */
+    lista_destruir(cola);
 }
