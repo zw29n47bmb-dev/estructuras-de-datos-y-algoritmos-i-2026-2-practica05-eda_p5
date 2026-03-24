17192c164e0ee425b0e32d461ce8a84dd8031b78
zw29n47bmb-dev <zw29n47bmb@privaterelay.appleid.com>
2026-03-24 15:04:36 -0700

    Update comments for push, pop, and top functions

--- pila/pila.h
+++ pila/pila.h
@@ -9,22 +9,13 @@ Pila* pila_crear();
 
 int pila_vacia(Pila* pila);
 
-/*
-TODO
-push debe insertar por tail
-*/
+/* push: insertar por tail */
 void pila_push(Pila* pila, int dato);
 
-/*
-TODO
-pop debe eliminar por tail
-*/
+/* pop: eliminar por tail */
 int pila_pop(Pila* pila);
 
-/*
-TODO
-regresa elemento superior (tail)
-*/
+/* top: regresar elemento superior (tail) */
 int pila_top(Pila* pila);
 
 void pila_destruir(Pila* pila);
